66a32d96f27cb83bb2a66983110675dad2a6cb5a
archie3DXR <archie.majumdar@gmail.com>
2024-02-08 14:47:44 +1100

    Update footer component with links and copyright information

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,8 +1,44 @@
+// function Footer() {
+//   // const instagram = 'https://www.instagram.com/thenourishednotebook/';
+
+//   return {};
 
 function Footer() {
-  // const instagram = 'https://www.instagram.com/thenourishednotebook/';
-
-  return {};
+	return (
+		<footer
+			style={{
+				display: "flex",
+				flexDirection: "column",
+				alignItems: "center",
+				padding: "20px",
+				backgroundColor: "white",
+			}}
+		>
+			<div style={{ display: "flex", gap: "20px", marginBottom: "10px" }}>
+				<a
+					href="/contact"
+					style={{ textDecoration: "none", color: "black" }}
+				>
+					Contact Us
+				</a>
+				<a
+					href="/about"
+					style={{ textDecoration: "none", color: "black" }}
+				>
+					About Us
+				</a>
+				<a
+					href="https://instagram.com"
+					style={{ textDecoration: "none", color: "black" }}
+				>
+					Instagram
+				</a>
+			</div>
+			<div style={{ color: "grey" }}>
+				<p>&copy; {new Date().getFullYear()} Privacy - Terms</p>
+			</div>
+		</footer>
+	)
 }
 
-export default Footer;
+export default Footer
